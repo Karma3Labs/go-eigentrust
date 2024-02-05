e4b51f3adf1aa9ddcdcbd87a26fdcc748d908b9f
Eugene Kim <ek@karma3labs.com>
2024-02-05 03:06:05 -0800

    Add NamedTrustVector, for use with stored pre-trust

--- pkg/basic/server/lockedtrust.go
+++ pkg/basic/server/lockedtrust.go
@@ -7,6 +7,28 @@ import (
 	"k3l.io/go-eigentrust/pkg/sparse"
 )
 
+type TrustVector struct {
+	vector    *sparse.Vector
+	timestamp big.Int
+	mutex     sync.Mutex
+}
+
+func NewTrustVectorWithContents(c *sparse.Vector) *TrustVector {
+	return &TrustVector{vector: c}
+}
+
+func NewTrustVector() *TrustVector {
+	return NewTrustVectorWithContents(sparse.NewVector(0, nil))
+}
+
+func (m *TrustVector) LockAndRun(
+	f func(vector *sparse.Vector, timestamp *big.Int),
+) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	f(m.vector, &m.timestamp)
+}
+
 type TrustMatrix struct {
 	matrix    *sparse.Matrix
 	timestamp big.Int
