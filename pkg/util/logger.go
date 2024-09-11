7b77e722deed9cbbbe01d5131fe0da287c5c7c13
Eugene Kim <ek@karma3labs.com>
2024-09-11 14:43:34 -0700

    feat: add helper to add caller info to zerologgers

--- pkg/util/logger.go
+++ pkg/util/logger.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"runtime"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -39,4 +40,36 @@ func (p *TimeLogger) Log(name string) {
 		Dur("cumulative", cumulative).
 		Msg("finished lap")
 	p.LapStart = now
+}
+
+// LoggerWithCallerAtDepth recovers the caller at the given stack depth
+// and adds it to the given logger under the "func", "file", and "line" keys.
+// depth is 0 for the caller of LoggerWithCallerAtDepth.
+// It returns the logger unmodified if the given stack frame doesn't exist.
+func LoggerWithCallerAtDepth(depth int, logger zerolog.Logger) zerolog.Logger {
+	pc, file, line, ok := runtime.Caller(depth + 1)
+	if !ok {
+		return logger
+	}
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return logger
+	}
+	return logger.With().
+		Str("file", file).
+		Int("line", line).
+		Str("func", fn.Name()).
+		Logger()
+}
+
+// LoggerWithCaller recovers the calling function of LoggerWithCaller
+// and adds it to the given logger under the "func", "file", and "line" keys.
+func LoggerWithCaller(logger zerolog.Logger) zerolog.Logger {
+	return LoggerWithCallerAtDepth(1, logger)
+}
+
+// AddCallerToLogger adds the calling function to the logger
+// under the "func", "file", and "line" keys.
+func AddCallerToLogger(logger zerolog.Logger) {
+	logger = LoggerWithCallerAtDepth(1, logger)
 }
